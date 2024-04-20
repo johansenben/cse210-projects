7300da1a94c112ded6b0581c19749d724b4090e3
Ben Johansen <johansenben12@gmail.com>
2024-04-20 13:47:01 -0600

    prep1-3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? (0-100%)");
+        string gradeText = Console.ReadLine();
+        int grade = int.Parse(gradeText);
+
+        char letter = ' ';
+        if (grade >= 90) 
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80) 
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70) 
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60) 
+        {
+            letter = 'D';
+        }
+        else if (grade < 60) 
+        {
+            letter = 'F';
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+
+        string gradeSign = "";
+        if (grade % 10 >= 7 && letter != 'A' && letter != 'F')
+        {
+            gradeSign = "+";
+        }
+        else if (grade % 10 < 7 && letter != 'F')
+        {
+            gradeSign = "-";
+        }
+        Console.WriteLine($"Your grade is {letter}{gradeSign}.");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while (true)
+        {
+            Random random = new Random();
+            int number = random.Next(1,100);
+            int guesses = 0;
+
+            while (true)
+            {
+                Console.Write("Guess: ");
+                int guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (guess == number)
+                {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine($"{guesses} guesses");
+                    break;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            Console.Write("Do you want to continue? (y/n) ");
+            string cont = Console.ReadLine();
+            if (cont != "y")
+            {
+                break;
+            }
+        }
     }
 }
