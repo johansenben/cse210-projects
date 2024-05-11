25f289785829ee5e98becb130e8255a68792aadc
Ben Johansen <johansenben12@gmail.com>
2024-05-11 12:11:39 -0600

    week3 prove v3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,6 +1,6 @@
 /*
     Exceeds requirements
-    -It only hide words taht aren't already hidden
+    -It only hide words that aren't already hidden
     -A prefix can be added before a word to prevent it from being hidden
         -If theprefix is '%', '%word' won't be hidden, but will always be displayed as 'word'
     -the user is able to choose to pick a verse from the list, input their own verse or use a randomely generated verse
